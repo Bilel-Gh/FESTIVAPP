20e042b9595ccf707b431ee68a3a82b044829da9
Bilel <bilelghandri@outlook.fr>
2022-08-11 01:21:01 +0200

    Preparing for deployment

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import AuthRoute from "./Routes/AuthRoute.js";
 import UserRoute from "./Routes/UserRoute.js";
 import PostRoute from "./Routes/PostRoute.js";
 import UploadRoute from "./Routes/UploadRoute.js";
+import path from "path";
 
 // ───────────────────────────────────────ROUTES─────────────────────────────────────────
 
