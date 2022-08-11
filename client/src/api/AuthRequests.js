4971d977d60dd15caa1ff562c5ef375eb6779b70
Bilel <bilelghandri@outlook.fr>
2022-08-11 13:33:26 +0200

    Preparing for deployment

--- client/src/api/AuthRequests.js
+++ client/src/api/AuthRequests.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+// const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://festiv-app-online.herokuapp.com/" });
 
 export const logIn= (formData)=> API.post('/auth/login',formData);
 export const signUp = (formData) => API.post('/auth/register', formData)
