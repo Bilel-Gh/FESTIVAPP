818af12457bbebed3191d470362cb323c816a660
Bilel <bilelghandri@outlook.fr>
2022-08-11 02:02:50 +0200

    Preparing for deployment

--- server/index.js
+++ server/index.js
@@ -56,5 +56,9 @@ if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   }
-  );
+  )
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
 }
